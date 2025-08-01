b7b0cfdbe4254878a8522c0e0aff66b874032f7b
Ternamdoo <jerryaliyese@gmail.com>
2025-08-01 13:25:00 +0100

    Migrate page HTML from script.js to index.html

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// const body = document.querySelector('body');
-// const h1 = document.createElement('h1');
-// h1.textContent = 'Library';
-// body.appendChild(h1)
-
+// Create an array to contain added books
 const myLibrary = [];
 
 // Initialize Book Constructor
@@ -19,55 +15,51 @@ function Book(title, author, pages, imageURL, readStatus, rating) {
   this.rating = rating;
 }
 
+// Function to add create a new book and add it to the book array
 function addBookToLibrary(title, author, pages, imageURL, readStatus, rating) {
   let book = new Book(title, author, pages, imageURL, readStatus, rating);
   myLibrary.push(book);
 }
 
 // Add some books to the library
-addBookToLibrary('The Alchemist', 'Paulo Coelho', 182, 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1654371463i/18144590.jpg', true, 3.92);
-addBookToLibrary(1984, 'George Orwell', 368, 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1657781256i/61439040.jpg', false, 4.2);
-addBookToLibrary('Animal Farm', 'George Orwell', 141, 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1325861570i/170448.jpg', false, 4.01);
-addBookToLibrary('The Hunger Games', 'Suzanne Collins', 374, 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1586722975i/2767052.jpg', false, 4.35);
+const books = [
+  ['The Alchemist', 'Paulo Coelho', 182, 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1654371463i/18144590.jpg', true, 3.92],
+  [1984, 'George Orwell', 368, 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1657781256i/61439040.jpg', false, 4.2],
+  ['Animal Farm', 'George Orwell', 141, 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1325861570i/170448.jpg', false, 4.01],
+  ['The Hunger Games', 'Suzanne Collins', 374, 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1586722975i/2767052.jpg', false, 4.35]
+];
+books.forEach( (book) => {
+  addBookToLibrary(...book);
+});
 
-// const bookShelf = document.createElement('div');
-// body.appendChild(bookShelf);
-// bookShelf.setAttribute('id', 'container');
+// Display each book information on the page
+const bookShelf = document.querySelector('#book-shelf');
 
-// myLibrary.forEach( (book) => {
-//   const bookCard = document.createElement('div');
+myLibrary.forEach( (book) => {
+  const bookCard = document.createElement('div');
 
-//   const bookCover = document.createElement('div');
-//   const coverImage = document.createElement('img');
-//   coverImage.setAttribute('src', book.imageURL);
-//   coverImage.setAttribute('alt', 'Book cover photo.');
-//   bookCover.append(coverImage);
+  const coverImage = document.createElement('img');
+  coverImage.setAttribute('src', book.imageURL);
+  coverImage.setAttribute('alt', 'Book cover photo.');
 
-//   const bookInfo = document.createElement('div');
-//   const bookTitle = document.createElement('div');
-//   const bookAuthor = document.createElement('div');
-//   const bookPages = document.createElement('div');
-//   const readStatus = document.createElement('div');
-//   const bookRating = document.createElement('div');
+  const bookInfo = document.createElement('div');
+  const bookTitle = document.createElement('div');
+  const bookAuthor = document.createElement('div');
+  const bookPages = document.createElement('div');
+  const readStatus = document.createElement('div');
+  const bookRating = document.createElement('div');
 
-//   bookTitle.textContent = book.title;
-//   bookAuthor.textContent = `by ${book.author}`;
-//   bookPages.textContent = `${book.pages} pages`;
-//   readStatus.textContent = book.readStatus === true ? 'Read' : 'Not Read';
-//   bookRating.textContent = `${book.rating}`;
-//   bookInfo.append(bookTitle, bookAuthor, bookPages, bookRating, readStatus);
-//   bookCard.append(bookCover, bookInfo);
-//   bookShelf.append(bookCard);
+  bookTitle.textContent = book.title;
+  bookAuthor.textContent = `by ${book.author}`;
+  bookPages.textContent = `${book.pages} pages`;
+  readStatus.textContent = book.readStatus === true ? 'Read' : 'Not Read';
+  bookRating.textContent = `${book.rating}`;
 
-//   // Add IDs and Classes to the elements for styling
-//   bookCard.setAttribute('class', 'card');
-//   bookCover.setAttribute('class', 'image');
-//   bookInfo.setAttribute('class', 'book-info');
-// })
+  bookInfo.append(bookTitle, bookAuthor, bookPages, bookRating, readStatus);
+  bookCard.append(coverImage, bookInfo);
+  bookShelf.append(bookCard);
 
-// body.append(bookShelf);
-
-
-// // Create a form modal
-// const modal = document.createElement('div');
-// const formCode = ``;
+  // Add IDs and Classes to the elements for styling
+  bookCard.setAttribute('class', 'card');
+  bookInfo.setAttribute('class', 'book-info');
+})
